0e45dd378b38ba3f9cc2380b689909c7087a6fdd
paleaura01 <99763057+paleaura01@users.noreply.github.com>
2023-06-19 02:34:48 -0400

    w
    s

--- fetcher.js
+++ fetcher.js
@@ -1,131 +1,135 @@
-var FeedMe = require("feedme");
-var request = require("request");
-var iconv = require("iconv-lite");
-
-var Fetcher = function(url, reloadInterval, encoding, logFeedWarnings, defaultLogo) {
-  var self = this;
-  if (reloadInterval < 1000) {
-    reloadInterval = 1000;
-  }
-
-  var reloadTimer = null;
-  var items = [];
-  var logo = defaultLogo;
-
-  var fetchFailedCallback = function() {};
-  var itemsReceivedCallback = function() {};
-
-  var fetchNews = function() {
-    clearTimeout(reloadTimer);
-    reloadTimer = null;
-    items = [];
-
-    var parser = new FeedMe();
-
-    parser.on("item", function(item) {
-      var title = item.title;
-      var description = item.description || item.summary || item.content || "";
-      var pubdate = item.pubdate || item.published || item.updated || item["dc:date"];
-      var url = item.url || item.link || "";
-
-      if (title && pubdate) {
-        var regex = /(<([^>]+)>)/ig;
-        description = description.toString().replace(regex, "");
-
-        items.push({
-          title: title,
-          description: description,
-          pubdate: pubdate,
-          url: url,
-          logo: logo,
-          enclosure: url
-        });
-      } else if (logFeedWarnings) {
-        console.log("Can't parse feed item:");
-        console.log(item);
-        console.log("Title: " + title);
-        console.log("Description: " + description);
-        console.log("Pubdate: " + pubdate);
-      }
-    });
-
-    parser.on("image", function(image) {
-      if (image.url) {
-        defaultLogo = image.url;
-      } else if (logFeedWarnings) {
-        console.log("Image parsing error.");
-      }
-    });
-
-    parser.on("end", function() {
-      self.broadcastItems();
-      scheduleTimer();
-    });
-
-    parser.on("error", function(error) {
-      fetchFailedCallback(self, error);
-      scheduleTimer();
-    });
-
-    var nodeVersion = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
-    var headers = {
-      "User-Agent": "Mozilla/5.0 (Node.js " + nodeVersion + ") MagicMirror/" + global.version + " (https://github.com/MichMich/MagicMirror/)",
-      "Cache-Control": "max-age=0, no-cache, no-store, must-revalidate",
-      "Pragma": "no-cache"
-    };
-
-    request({ uri: url, encoding: null, headers: headers })
-      .on("error", function(error) {
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  var FeedMe = require("feedme");
+  var request = require("request");
+  var iconv = require("iconv-lite");
+  
+  var Fetcher = function(url, reloadInterval, encoding, logFeedWarnings, defaultLogo) {
+    var self = this;
+    if (reloadInterval < 1000) {
+      reloadInterval = 1000;
+    }
+  
+    var reloadTimer = null;
+    var items = [];
+    var logo = defaultLogo;
+  
+    var fetchFailedCallback = function() {};
+    var itemsReceivedCallback = function() {};
+  
+    var fetchNews = function() {
+      clearTimeout(reloadTimer);
+      reloadTimer = null;
+      items = [];
+  
+      var parser = new FeedMe();
+  
+      parser.on("item", function(item) {
+        var title = item.title;
+        var description = item.description || item.summary || item.content || "";
+        var pubdate = item.pubdate || item.published || item.updated || item["dc:date"];
+        var url = item.url || item.link || "";
+  
+        if (title && pubdate) {
+          var regex = /(<([^>]+)>)/ig;
+          description = description.toString().replace(regex, "");
+  
+          items.push({
+            title: title,
+            description: description,
+            pubdate: pubdate,
+            url: url,
+            logo: logo,
+            enclosure: url
+          });
+        } else if (logFeedWarnings) {
+          console.log("Can't parse feed item:");
+          console.log(item);
+          console.log("Title: " + title);
+          console.log("Description: " + description);
+          console.log("Pubdate: " + pubdate);
+        }
+      });
+  
+      parser.on("image", function(image) {
+        if (image.url) {
+          defaultLogo = image.url;
+        } else if (logFeedWarnings) {
+          console.log("Image parsing error.");
+        }
+      });
+  
+      parser.on("end", function() {
+        self.broadcastItems();
+        scheduleTimer();
+      });
+  
+      parser.on("error", function(error) {
         fetchFailedCallback(self, error);
         scheduleTimer();
-      })
-      .pipe(iconv.decodeStream(encoding))
-      .pipe(parser);
-  };
-
-  var scheduleTimer = function() {
-    clearTimeout(reloadTimer);
-    reloadTimer = setTimeout(function() {
+      });
+  
+      var nodeVersion = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
+      var headers = {
+        "User-Agent": "Mozilla/5.0 (Node.js " + nodeVersion + ") MagicMirror/" + global.version + " (https://github.com/MichMich/MagicMirror/)",
+        "Cache-Control": "max-age=0, no-cache, no-store, must-revalidate",
+        "Pragma": "no-cache"
+      };
+  
+      request({ uri: url, encoding: null, headers: headers })
+        .on("error", function(error) {
+          fetchFailedCallback(self, error);
+          scheduleTimer();
+        })
+        .pipe(iconv.decodeStream(encoding))
+        .pipe(parser);
+    };
+  
+    var scheduleTimer = function() {
+      clearTimeout(reloadTimer);
+      reloadTimer = setTimeout(function() {
+        fetchNews();
+      }, reloadInterval);
+    };
+  
+    this.setReloadInterval = function(interval) {
+      if (interval > 1000 && interval < reloadInterval) {
+        reloadInterval = interval;
+      }
+    };
+  
+    this.startFetch = function() {
       fetchNews();
-    }, reloadInterval);
+    };
+  
+    this.broadcastItems = function() {
+      if (items.length <= 0) {
+        return;
+      }
+      itemsReceivedCallback(self);
+    };
+  
+    this.onReceive = function(callback) {
+      itemsReceivedCallback = callback;
+    };
+  
+    this.onError = function(callback) {
+      fetchFailedCallback = callback;
+    };
+  
+    this.url = function() {
+      return url;
+    };
+  
+    this.logo = function() {
+      return logo;
+    };
+  
+    this.items = function() {
+      return items;
+    };
   };
-
-  this.setReloadInterval = function(interval) {
-    if (interval > 1000 && interval < reloadInterval) {
-      reloadInterval = interval;
-    }
-  };
-
-  this.startFetch = function() {
-    fetchNews();
-  };
-
-  this.broadcastItems = function() {
-    if (items.length <= 0) {
-      return;
-    }
-    itemsReceivedCallback(self);
-  };
-
-  this.onReceive = function(callback) {
-    itemsReceivedCallback = callback;
-  };
-
-  this.onError = function(callback) {
-    fetchFailedCallback = callback;
-  };
-
-  this.url = function() {
-    return url;
-  };
-
-  this.logo = function() {
-    return logo;
-  };
-
-  this.items = function() {
-    return items;
-  };
-};
-
-module.exports = Fetcher;
+  
+  module.exports = Fetcher;
+  
+}
--- node_helper.js
+++ node_helper.js
@@ -1,68 +1,71 @@
-var NodeHelper = require("node_helper");
-var validUrl = require("valid-url");
-var Fetcher = require("./fetcher.js");
-
-module.exports = NodeHelper.create({
-  start: function() {
-    console.log("Starting module: " + this.name);
-    this.fetchers = {};
-  },
-
-  socketNotificationReceived: function(notification, payload) {
-    if (notification === "ADD_FEED") {
-      this.createFetcher(payload.feed, payload.config);
-      return;
-    }
-  },
-
-  createFetcher: function(feed, config) {
-    var self = this;
-    var url = feed.url || "";
-    var encoding = feed.encoding || "UTF-8";
-    var reloadInterval = feed.reloadInterval || config.reloadInterval || 5 * 60 * 1000;
-
-    if (!validUrl.isUri(url)) {
-      self.sendSocketNotification("INCORRECT_URL", url);
-      return;
-    }
-
-    var fetcher;
-    if (typeof self.fetchers[url] === "undefined") {
-      console.log("Create new news fetcher for url: " + url + " - Interval: " + reloadInterval + " logo = " + feed.customLogo);
-      fetcher = new Fetcher(url, reloadInterval, encoding, config.logFeedWarnings, feed.customLogo);
-
-      fetcher.onReceive(function(fetcher) {
-        var items = fetcher.items();
-        for (var i in items) {
-          var item = items[i];
-          // item.image = this.feed_var_in_function.image;
-        }
-        self.broadcastFeeds();
-      }.bind({ feed_var_in_function: feed }));
-
-      fetcher.onError(function(fetcher, error) {
-        self.sendSocketNotification("FETCH_ERROR", {
-          url: fetcher.url(),
-          error: error
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  var NodeHelper = require("node_helper");
+  var validUrl = require("valid-url");
+  var Fetcher = require("./fetcher.js");
+  
+  module.exports = NodeHelper.create({
+    start: function() {
+      console.log("Starting module: " + this.name);
+      this.fetchers = {};
+    },
+  
+    socketNotificationReceived: function(notification, payload) {
+      if (notification === "ADD_FEED") {
+        this.createFetcher(payload.feed, payload.config);
+        return;
+      }
+    },
+  
+    createFetcher: function(feed, config) {
+      var self = this;
+      var url = feed.url || "";
+      var encoding = feed.encoding || "UTF-8";
+      var reloadInterval = feed.reloadInterval || config.reloadInterval || 5 * 60 * 1000;
+  
+      if (!validUrl.isUri(url)) {
+        self.sendSocketNotification("INCORRECT_URL", url);
+        return;
+      }
+  
+      var fetcher;
+      if (typeof self.fetchers[url] === "undefined") {
+        console.log("Create new news fetcher for url: " + url + " - Interval: " + reloadInterval + " logo = " + feed.customLogo);
+        fetcher = new Fetcher(url, reloadInterval, encoding, config.logFeedWarnings, feed.customLogo);
+  
+        fetcher.onReceive(function(fetcher) {
+          var items = fetcher.items();
+          for (var i in items) {
+            var item = items[i];
+            // item.image = this.feed_var_in_function.image;
+          }
+          self.broadcastFeeds();
+        }.bind({ feed_var_in_function: feed }));
+  
+        fetcher.onError(function(fetcher, error) {
+          self.sendSocketNotification("FETCH_ERROR", {
+            url: fetcher.url(),
+            error: error
+          });
         });
-      });
-
-      self.fetchers[url] = fetcher;
-    } else {
-      console.log("Use existing news fetcher for url: " + url);
-      fetcher = self.fetchers[url];
-      fetcher.setReloadInterval(reloadInterval);
-      fetcher.broadcastItems();
+  
+        self.fetchers[url] = fetcher;
+      } else {
+        console.log("Use existing news fetcher for url: " + url);
+        fetcher = self.fetchers[url];
+        fetcher.setReloadInterval(reloadInterval);
+        fetcher.broadcastItems();
+      }
+  
+      fetcher.startFetch();
+    },
+  
+    broadcastFeeds: function() {
+      var feeds = {};
+      for (var f in this.fetchers) {
+        feeds[f] = this.fetchers[f].items();
+      }
+      this.sendSocketNotification("NEWS_ITEMS", feeds);
     }
-
-    fetcher.startFetch();
-  },
-
-  broadcastFeeds: function() {
-    var feeds = {};
-    for (var f in this.fetchers) {
-      feeds[f] = this.fetchers[f].items();
-    }
-    this.sendSocketNotification("NEWS_ITEMS", feeds);
-  }
-});
+  });
+}
